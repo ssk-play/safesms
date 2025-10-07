b45a4490c362d119d479bfa4ba70f062bec8cddd
ssk <developer.kss@gmail.com>
2025-10-07 11:09:07 +0900

    chore: Bump version to 0.1.2 (3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "ssk.kidssms"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "0.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
